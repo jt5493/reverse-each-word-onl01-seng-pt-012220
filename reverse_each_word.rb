b22d0cf95a3fc4734483990046becfc34058383f
Jordon Terry <jt5493@hotmail.com>
2020-01-31 05:13:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 require "pry"
 
 def reverse_each_word(string)
-  string.split(" ")
-binding.pry
-  string.collect do |s|
-    string.reverse
+  string.split.collect do |s|
+    s.reverse.join    
   end
 end
